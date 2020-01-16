6747e7d381c867ca267acf80dbc5c0998211ee23
johnptmcdonald <johnptmcdonald@gmail.com>
2020-01-16 12:37:11 -0500

    adds demo cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: "Neat Tree",
+      username: "@nature",
+      imageUrl: "assets/tree.jpeg",
+      content: "I saw this neat tree today"
+    },
+    {
+      title: "Snowy Mountain",
+      username: "@mountain_lover",
+      imageUrl: "assets/mountain.jpeg",
+      content: "I saw this mountain today"
+    },
+    {
+      title: "Neat Tree",
+      username: "@rider",
+      imageUrl: "assets/biking.jpeg",
+      content: "I did some biking today"
+    }
+  ];
 }
